f0822d9ca10bdf6f1685bd9cef2c419c605e4481
Daniel <rainstormstudio@yahoo.com>
2019-11-29 10:23:17 -0500

    cleanmemory

---
+++ cleanmemory/cleanmem.asm
@@ -0,0 +1,24 @@
+	processor 6502
+	SEG code
+	ORG $F000 		; define the code origin at $F000
+
+START:
+	SEI			; disable interrupts
+	CLD			; disable the BCD decimal math mode
+	LDX #$FF		; loads the X register with #$FF
+	TXS			; transfer X register to S(tack) register
+	
+;;; Clear the Zero Page region ($00 to $FF)
+;;; Meaning the entire TIA register space and also RAM
+	LDA #0			; A = 0
+	LDX #$FF		; X = #$FF
+
+MEMLOOP:
+	STA $0,X		; store zero at address $0 + X
+	DEX			; X--
+	BNE MEMLOOP		; loop until X == 0 (Z-flag set)
+
+;;; Fill ROM size to exactly 4kb
+	ORG $FFFC
+	.WORD START		; reset vector at $FFFC (where program starts)
+	.WORD START		; interrupt vector at $FFFE (unused in VCS)
