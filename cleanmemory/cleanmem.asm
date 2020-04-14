0d765d137a0ae46f0cc766626649e6638a227407
Daniel <rainstormstudio@yahoo.com>
2020-04-14 17:25:54 -0400

    dasm

--- cleanmemory/cleanmem.asm
+++ cleanmemory/cleanmem.asm
@@ -1,5 +1,5 @@
-	processor 6502
-	SEG code
+	PROCESSOR 6502		; set the processor
+	SEG CODE		; segment code
 	ORG $F000 		; define the code origin at $F000
 
 START:
@@ -7,14 +7,14 @@ START:
 	CLD			; disable the BCD decimal math mode
 	LDX #$FF		; loads the X register with #$FF
 	TXS			; transfer X register to S(tack) register
-	
+
 ;;; Clear the Zero Page region ($00 to $FF)
 ;;; Meaning the entire TIA register space and also RAM
 	LDA #0			; A = 0
 	LDX #$FF		; X = #$FF
 
 MEMLOOP:
-	STA $0,X		; store zero at address $0 + X
+	STA $0,X		; store A(0) at address $0 + X
 	DEX			; X--
 	BNE MEMLOOP		; loop until X == 0 (Z-flag set)
 
@@ -22,3 +22,4 @@ MEMLOOP:
 	ORG $FFFC
 	.WORD START		; reset vector at $FFFC (where program starts)
 	.WORD START		; interrupt vector at $FFFE (unused in VCS)
+	
